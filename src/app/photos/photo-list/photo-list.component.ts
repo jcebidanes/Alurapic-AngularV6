1e87de28c097fb426ecc7d051325a04ff9860db2
Jhonnatan Cebidanes <jcebidanes@gmail.com>
2018-08-30 00:24:45 -0400

    Projeto alurapic Angular v6 até aula 4 completo

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Photo } from '../photo';
+import { PhotosService } from '../photos.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'ap-photo-list',
+    templateUrl: './photo-list.component.html',
+    styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+    public photos: Photo[];
+
+    constructor(
+        private photoService: PhotosService,
+        private activateRoute: ActivatedRoute
+    ) { }
+
+    ngOnInit(): void {
+
+        const userName = this.activateRoute.snapshot.params.userName;
+        this.photoService.listFromUser(userName).subscribe(
+            photos => this.photos = photos
+        );
+    }
+
+}
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from '@angular/common';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+
+@NgModule({
+    declarations: [
+        PhotoComponent,
+        PhotoListComponent,
+        PhotoFormComponent
+    ],
+    imports: [
+        HttpClientModule,
+        CommonModule
+    ],
+    exports: []
+})
+export class PhotosModule { }
